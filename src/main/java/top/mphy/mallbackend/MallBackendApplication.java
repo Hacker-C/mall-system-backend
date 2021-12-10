5699db0edb18cf2310c2f41490ed2c5384dbb650
Hacker-C <1507559148@qq.com>
2021-12-10 08:21:21 +0800

    init: 项目初始化

---
+++ src/main/java/top/mphy/mallbackend/MallBackendApplication.java
@@ -0,0 +1,13 @@
+package top.mphy.mallbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/mphy/mallbackend/MallBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package top.mphy.mallbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
